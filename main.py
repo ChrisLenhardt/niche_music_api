fce728b35b10f187b7ed899d6da2c035e4d24d85
Christopher Lenhardt <christopher.e.lenhardt@gmail.com>
2026-02-22 03:38:06 -0500

    Add CORS

--- main.py
+++ main.py
@@ -6,8 +6,25 @@ from dotenv import load_dotenv
 import os
 
 from util.spotify_helpers import get_artist_image
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000/",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 load_dotenv()
 
